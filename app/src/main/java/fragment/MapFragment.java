915aef98901d8375df7b5e281742018e3bef6f63
DuZeming <1656499605@qq.com>
2017-06-05 21:48:46 +0800

    Initial commit

---
+++ app/src/main/java/fragment/MapFragment.java
@@ -0,0 +1,209 @@
+package fragment;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MyLocationStyle;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.route.BusRouteResult;
+import com.amap.api.services.route.DriveRouteResult;
+import com.amap.api.services.route.RideRouteResult;
+import com.amap.api.services.route.RouteSearch;
+import com.amap.api.services.route.WalkRouteResult;
+import com.example.duzeming.carhelper.R;
+
+/**
+ * Created by DuZeming on 2017/4/27.
+ */
+public class MapFragment extends Fragment implements com.amap.api.maps2d.LocationSource,AMapLocationListener, com.amap.api.maps2d.AMap.OnMapClickListener
+, com.amap.api.maps2d.AMap.OnMarkerClickListener, com.amap.api.maps2d.AMap.OnInfoWindowClickListener, com.amap.api.maps2d.AMap.InfoWindowAdapter,RouteSearch.OnRouteSearchListener{
+    private com.amap.api.maps2d.MapView mapView;
+    private com.amap.api.maps2d.AMap aMap;
+    private com.amap.api.maps2d.UiSettings mUiSettings;
+    private OnLocationChangedListener mListener;
+    private AMapLocationClient mapLocationClient;
+    private AMapLocationClientOption mapLocationClientOption;
+
+
+    private Context mContext;
+    private RouteSearch mRouteSearch;
+    private DriveRouteResult mDriveRouteResult;
+    private LatLonPoint mStartPoint = new LatLonPoint(39.942295, 116.335891);//起点，116.335891,39.942295
+    private LatLonPoint mEndPoint = new LatLonPoint(39.995576, 116.481288);//终点，116.481288,39.995576
+    private final int ROUTE_TYPE_DRIVE = 2;
+
+    private RelativeLayout mBottomLayout, mHeadLayout;
+    private TextView mRotueTimeDes, mRouteDetailDes;
+    private ProgressDialog progDialog = null;// 搜索时进度条
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        mapView = (com.amap.api.maps2d.MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+
+        if (aMap == null){
+            aMap=mapView.getMap();
+            mUiSettings=aMap.getUiSettings();
+            setUpMap();
+        }
+        mUiSettings.setScrollGesturesEnabled(true);
+        mUiSettings.setZoomGesturesEnabled(true);
+
+
+        return view;
+    }
+    private void setUpMap() {
+        // 自定义系统定位小蓝点
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory
+                .fromResource(R.drawable.location_marker));// 设置小蓝点的图标
+        myLocationStyle.strokeColor(Color.BLACK);// 设置圆形的边框颜色
+        myLocationStyle.radiusFillColor(Color.argb(100, 0, 0, 180));// 设置圆形的填充颜色
+        // myLocationStyle.anchor(int,int)//设置小蓝点的锚点
+        myLocationStyle.strokeWidth(1.0f);// 设置圆形的边框粗细
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setLocationSource(this);// 设置定位监听
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+        // aMap.setMyLocationType()
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        if (mListener != null && aMapLocation != null) {
+            if (aMapLocation != null
+                    && aMapLocation.getErrorCode() == 0) {
+                mListener.onLocationChanged(aMapLocation);// 显示系统小蓝点
+            } else {
+                String errText = "定位失败," + aMapLocation.getErrorCode()+ ": " + aMapLocation.getErrorInfo();
+                Log.e("AmapErr", errText);
+            }
+        }
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        if (mapLocationClient == null) {
+            mapLocationClient = new AMapLocationClient(getActivity());
+            mapLocationClientOption = new AMapLocationClientOption();
+            //设置定位监听
+            mapLocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mapLocationClientOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位参数
+            mapLocationClient.setLocationOption(mapLocationClientOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            mapLocationClient.startLocation();
+        }
+    }
+
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mapLocationClient != null) {
+            mapLocationClient.stopLocation();
+            mapLocationClient.onDestroy();
+        }
+        mapLocationClient = null;
+    }
+
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
+    }
+
+    @Override
+    public void onBusRouteSearched(BusRouteResult busRouteResult, int i) {
+
+    }
+
+    @Override
+    public void onDriveRouteSearched(DriveRouteResult driveRouteResult, int i) {
+
+    }
+
+    @Override
+    public void onWalkRouteSearched(WalkRouteResult walkRouteResult, int i) {
+
+    }
+
+    @Override
+    public void onRideRouteSearched(RideRouteResult rideRouteResult, int i) {
+
+    }
+}
---
+++ app/src/main/java/fragment/NavigationFragment.java
@@ -0,0 +1,70 @@
+package fragment;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+
+import com.example.duzeming.carhelper.R;
+
+/**
+ * Created by DuZeming on 2017/4/27.
+ */
+public class NavigationFragment extends Fragment implements View.OnClickListener {
+
+    private RadioButton toMap;
+    private RadioButton toNew;
+    private RadioButton toMy;
+    private FragmentManager fragmentManager;
+    private android.support.v4.app.FragmentTransaction transaction;
+    MapFragment mapFragment;
+    NewFragment newFragment;
+    MyFragment myFragment;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_navigation,container,false);
+
+
+        fragmentManager = getActivity().getSupportFragmentManager();
+        mapFragment = new MapFragment();
+        newFragment = new NewFragment();
+        myFragment = new MyFragment();
+
+        fragmentManager.beginTransaction().add(R.id.fra_up,mapFragment).add(R.id.fra_up,newFragment)
+                .add(R.id.fra_up,myFragment).commit();
+        fragmentManager.beginTransaction().hide(newFragment).hide(myFragment).commit();
+
+        toMap = (RadioButton) view.findViewById(R.id.radio_toMap);
+        toNew= (RadioButton) view.findViewById(R.id.radio_toNew);
+        toMy= (RadioButton) view.findViewById(R.id.radio_toMy);
+        toMap.setOnClickListener(this);
+        toNew.setOnClickListener(this);
+        toMy.setOnClickListener(this);
+
+
+        return  view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        transaction = fragmentManager.beginTransaction();
+        switch (v.getId()) {
+            case R.id.radio_toMap:
+                transaction.hide(newFragment).hide(myFragment).show(mapFragment).commit();
+                break;
+            case R.id.radio_toNew:
+                transaction.hide(mapFragment).hide(myFragment).show(newFragment).commit();
+                break;
+            case R.id.radio_toMy:
+                transaction.hide(newFragment).hide(mapFragment).show(myFragment).commit();
+                break;
+        }
+    }
+}
